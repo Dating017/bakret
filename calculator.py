cee0d4452ba8e19afa0908d9b593609304d13780
Destiny <destinydating2017@gmail.com>
2020-03-18 12:03:32 -1200

    message

---
+++ calculator.py
@@ -0,0 +1,7 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/USER/AppData/Local/Programs/Python/Python38-32/calculator.py
+what is your number:16
+4.0
+>>> 
